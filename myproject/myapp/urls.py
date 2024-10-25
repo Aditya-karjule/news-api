2a90ef6221ca1438842f671fb67e96784161f539
Aditya-karjule <adityakarjule@gmail.com>
2024-10-25 13:50:29 +0530

    Delete myproject directory

--- myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from myapp import views
-# from .views import hi
-
-urlpatterns = [
-    path('',views.news_view, name = 'news_view')
-]
